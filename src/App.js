2383f9ea81f088d966752461b367b02bd7dae378
Arthur Nonaka <arthur.nonaka@fiscaltech.com.br>
2023-08-11 20:38:42 -0300

    Adição do useEffect

--- src/App.js
+++ src/App.js
@@ -1,49 +1,52 @@
-import React from 'react'
-import { tab } from '@testing-library/user-event/dist/tab';
 import './App.css'
-import { initializeApp } from 'firebase/app'
-import { getDatabase, ref, push, onValue } from 'firebase/database' 
-import Header from "./components/Header"
-import ChartRender from "./components/ChartRender"
+
+import {tab} from '@testing-library/user-event/dist/tab';
+import {initializeApp} from 'firebase/app'
+import {getDatabase, onValue, push, ref} from 'firebase/database'
+import React from 'react'
+
+import ChartRender from './components/ChartRender'
+import Header from './components/Header'
 
 const appSettings = {
-  databaseURL: "https://controle-de-fatura-default-rtdb.firebaseio.com/"
-}
-const app = initializeApp(appSettings)
+  databaseURL: 'https://controle-de-fatura-default-rtdb.firebaseio.com/'
+} const app = initializeApp(appSettings)
 const database = getDatabase(app)
-const expensesInDB = ref(database, "expensesControl")
+const expensesInDB = ref(database, 'expensesControl')
 function App() {
   const [expenseArthur, setExpenseArthur] = React.useState(0)
   const [expenseMaysa, setExpenseMaysa] = React.useState(0)
   const [expenseToSplit, setExpenseToSplit] = React.useState(0)
 
-  onValue(expensesInDB, function(snapshot) {
-    clearTable()
-    let itemsArray = Object.values(snapshot.val())
-    let newExpenseArthur = 0
-    let newExpenseMaysa = 0
-    let newExpenseToSplit = 0
-    for (let i = 0; i < itemsArray.length; i++) {      
-        addExpenseToTable(itemsArray[i])
-        if(itemsArray[i][0].owner == "Arthur") {
-          newExpenseArthur += parseFloat(itemsArray[i][0].value)
-        } else if(itemsArray[i][0].owner == "Maysa") {
-          newExpenseMaysa += parseFloat(itemsArray[i][0].value)
-        } else if(itemsArray[i][0].owner == "Dividido") {
-          newExpenseToSplit += parseFloat(itemsArray[i][0].value)
-        }
-    }
-    // setExpenseArthur(newExpenseArthur)
-    // setExpenseMaysa(newExpenseMaysa)
-    // setExpenseToSplit(newExpenseToSplit)
-  })
+  React.useEffect(() => {onValue(expensesInDB, function(snapshot) {
+                    clearTable()
+                    let itemsArray = Object.values(snapshot.val())
+                    let newExpenseArthur = 0
+                    let newExpenseMaysa = 0
+                    let newExpenseToSplit = 0
+                    for (let i = 0; i < itemsArray.length; i++) {
+                      addExpenseToTable(itemsArray[i])
+                      if (itemsArray[i][0].owner == 'Arthur') {
+                        newExpenseArthur += parseFloat(itemsArray[i][0].value)
+                      }
+                      else if (itemsArray[i][0].owner == 'Maysa') {
+                        newExpenseMaysa += parseFloat(itemsArray[i][0].value)
+                      }
+                      else if (itemsArray[i][0].owner == 'Dividido') {
+                        newExpenseToSplit += parseFloat(itemsArray[i][0].value)
+                      }
+                    }
+                    setExpenseArthur(newExpenseArthur)
+                    setExpenseMaysa(newExpenseMaysa)
+                    setExpenseToSplit(newExpenseToSplit)
+                  })}, [])
 
   return (
-    <div className="App">
+    <div className='App'>
       <Header />
       <div className='charts-section'>
-        {/* <ChartRender soloValue = {expenseArthur} splitValue = {expenseToSplit} name = "Arthur"/>
-        <ChartRender soloValue = {expenseMaysa} splitValue = {expenseToSplit} name = "Maysa"/> */}
+        <ChartRender soloValue = {expenseArthur} splitValue = {expenseToSplit} name = 'Arthur'/>
+        <ChartRender soloValue = {expenseMaysa} splitValue = {expenseToSplit} name = 'Maysa'/>
       </div>
       <table id="table">
         <tr>
@@ -55,18 +58,18 @@ function App() {
       </table>
       <div id="input">
         <input id='date-input' placeholder='Data' type="date" max={getTodayDate()}></input>
-        <input id='location-input' placeholder='Lugar' type="text"></input>
+        <input id='location-input' placeholder='Lugar' type='text'></input>
         <form>
           <input type="radio" id="owner-arthur" className="input-owner" name="input-radio" value="Arthur" />
-          <label for="owner-arthur">Arthur</label><br />
-          <input type="radio" id="owner-maysa" className="input-owner" name="input-radio" value="Maysa" />
-          <label for="owner-maysa">Maysa</label><br />
-          <input type="radio" id="owner-split" className="input-owner" name="input-radio" value="Dividido" />
-          <label for="owner-split">Dividido</label>
+          <label for='owner-arthur'>Arthur</label><br />
+          <input type='radio' id='owner-maysa' className='input-owner' name='input-radio' value='Maysa' />
+          <label for='owner-maysa'>Maysa</label><br />
+          <input type='radio' id='owner-split' className='input-owner' name='input-radio' value='Dividido' />
+          <label for='owner-split'>Dividido</label>
         </form>
-        <input id='value-input' placeholder='Valor' type="number"></input>
+        <input id='value-input' placeholder='Valor' type='number'></input>
       </div>
-      <span id="check-input"></span>
+      <span id='check-input'></span>
       <button className='add-btn' onClick={handleAddButon}>Add</button>
       <button className='delete-btn' onClick={clearTable}>Delete all</button>
     </div>
@@ -75,15 +78,15 @@ function App() {
 
 
 function clearTable() {
-  const table = document.getElementById("table")
-  for(let i = table.rows.length - 1; i > 0; i--) {
+  const table = document.getElementById('table')
+  for (let i = table.rows.length - 1; i > 0; i--) {
     console.log('Deleting row at index:', i);
     table.deleteRow(i)
   }
 }
 
 function addExpenseToTable(props) {
-  const table = document.getElementById("table")
+  const table = document.getElementById('table')
   const row = table.insertRow(1)
   const date = row.insertCell(0)
   const location = row.insertCell(1)
@@ -96,35 +99,35 @@ function addExpenseToTable(props) {
 }
 
 function handleAddButon() {
-  const dateInput = document.getElementById("date-input").value
-  let dateValue = ""
-  if(dateInput === "") {
+  const dateInput = document.getElementById('date-input').value
+  let dateValue = ''
+  if (dateInput === '') {
     dateValue = getTodayDate()
-  } else {
+  }
+  else {
     dateValue = dateInput
   }
-  const locationInput = document.getElementById("location-input").value
+  const locationInput = document.getElementById('location-input').value
   const selectedOwner = document.querySelector('.input-owner:checked')
-  const valueInput = document.getElementById("value-input").value
-  const inputOwner = selectedOwner ? selectedOwner.value : "Dividido";
-  const table = document.getElementById("table")
+  const valueInput = document.getElementById('value-input').value
+  const inputOwner = selectedOwner ? selectedOwner.value : 'Dividido';
   const inputObject = [{
     date: dateValue,
     place: locationInput,
     owner: inputOwner,
     value: valueInput
-  }]
-  if(valueInput > 0) {
+  }] if (valueInput > 0) {
     push(expensesInDB, inputObject)
     resetInputFields()
-  } else {
-    const checkInput = document.getElementById("check-input")
-    checkInput.textContent = "Qual o valor?"
+  }
+  else {
+    const checkInput = document.getElementById('check-input')
+    checkInput.textContent = 'Qual o valor?'
   }
 }
 
 function handleDeleteButon() {
-  const countEl = document.getElementById("count")
+  const countEl = document.getElementById('count')
   countEl.textContent = 0
 }
 
@@ -137,12 +140,12 @@ function getTodayDate() {
 }
 
 function resetInputFields() {
-  const dateInput = document.getElementById("date-input")
-  const locationInput = document.getElementById("location-input")
-  const valueInput = document.getElementById("value-input")
+  const dateInput = document.getElementById('date-input')
+  const locationInput = document.getElementById('location-input')
+  const valueInput = document.getElementById('value-input')
 
-  dateInput.value = ""
-  locationInput.value = ""
-  valueInput.value = ""
+  dateInput.value = ''
+  locationInput.value = ''
+  valueInput.value = ''
 }
 export default App;
